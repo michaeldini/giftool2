3e92bdf7f58d63868a359995851f4c4cf10e3721
mdini <michaeldini@gmail.com>
2025-05-19 15:20:39 -0400

    Refactor main function to comment out Flask integration and enable Toga application launch

--- src/giftool2/main.py
+++ src/giftool2/main.py
@@ -1,7 +1,13 @@
 
 def main():
-    from .app import app
-    app.run()
-
+    # flask
+    # from .app import app
+    # app.run()
+    
+    # toga
+    from .toga_app import main
+    app = main()
+    app.main_loop()
+    
 if __name__ == "__main__":
     main()
